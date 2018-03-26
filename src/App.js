9629880a66ae812f50efc077262c086e5a15c061
DESKTOP-2EJNM36\yurik <pyurik44@gmail.com>
2018-03-27 00:44:07 +0300

    Weather Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "2b76d680bc3d595d0d890976b809fd1d";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Портфолио</h1>
         </header>
-        
+        <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
---
+++ src/components/Titles.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Titles extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Weathear Finder</h1>
+        <p>Find out trmperature, conditions and more...</p>
+      </div>
+    );
+  }
+}
