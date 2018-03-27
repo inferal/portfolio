bf9a14928e638218ffb41d7ec13cd8414699f15a
Yurik <pyurik44@gmail.com>
2018-03-27 16:56:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,33 @@ import Weather from "./components/Weather";
 const API_KEY = "2b76d680bc3d595d0d890976b809fd1d";
 
 class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_all = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
+    const data = await api_all.json();
+    console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
+  };
+
   render() {
     return (
       <div>
@@ -15,8 +42,15 @@ class App extends Component {
           <h1 className="App-title">Портфолио</h1>
         </header>
         <Titles />
-        <Form />
-        <Weather />
+        <Form getWeather={this.getWeather} />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
