b8c1249c93095f0392e0e74c796bfdcc203d07f6
inferal <pyurik44@gmail.com>
2018-03-29 10:59:22 +0300

    Правка

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
 export default class Form extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <form onSubmit={this.props.getWeather}>
-                <input type="text" name="city" placeholder="City..."/>
-                <input type="text" name="country" placeholder="Country..."/>
-                <button>Get Weather</button>
+                <input type="text" name="city" placeholder="City..." />
+                <input type="text" name="country" placeholder="Country..." />
+                <button>Get Weathe</button>
             </form>
         )
     }
